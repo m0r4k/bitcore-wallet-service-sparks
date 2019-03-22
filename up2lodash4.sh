27946c39514e9b7f6c87f80dffca859c1a1b6ac8
m0r4k <mor4k@outlook.com>
2019-03-22 16:06:50 +0100

    u2ld4.sh

--- up2lodash4.sh
+++ up2lodash4.sh
@@ -5,3 +5,4 @@ grep -rl lib/ -e "_.padRight" | xargs sed -i s@'_.padRight'@'_.padEnd@g'
 grep -rl lib/ -e "_.indexBy" | xargs sed -i s@'_.indexBy'@'_.keyBy@g'
 
 sed s@'"lodash": "^3.10.1"'@'"lodash": ">=4.7.11"'@g -i package.json
+mongo bws --eval "printjson(db.dropDatabase())"
